6d94e40c77c6418f1881fe146e042d89f0c09d07
Douglas Jorge Vizzacchi <douglas@vizzacchi.com.br>
2023-11-14 09:17:44 -0300

    primeiro commit

---
+++ Projeto-Tarefa/backend/src/index.ts
@@ -0,0 +1,13 @@
+import 'reflect-metadata';
+import * as express from 'express';
+import { AppDataSource } from './data-source';
+import * as bodyParser from 'body-parser';
+import routes from './routes';
+
+AppDataSource.initialize().then(async()=>{
+  const app = express();
+
+  app.use(bodyParser.json());
+  app.use(routes);
+  app.listen(3333);
+});
